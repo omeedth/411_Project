185c6861032973f51898da52b4b99934e272e0dd
David Wang <djwang826@gmail.com>
2019-10-25 11:35:38 -0400

    added bootstrap and modified home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
-
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.scss';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
@@ -9,14 +9,36 @@ import { HashRouter as Router } from 'react-router-dom';
 // Inside of your component's `render` method
 function App() {
   return (
-    <>
+    <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="https://codingthesmartway.com" target="_blank">
+              <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
+            </a>
+            <Link to="/" className="navbar-brand">MERN-Stack 411 App</Link>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">LandingPage</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <br/>
+          <Route path="/" exact component={LandingPage} />
+        </div>
+      </Router>
+  );
+}
+
+export default hot(App);
+
+/**
+ * <>
       <Router>                
           <Switch>
             <Route exact path="/" component={LandingPage} />
           </Switch>        
       </Router>
     </>
-  );
-}
-
-export default hot(App);
+ */
