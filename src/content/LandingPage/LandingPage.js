b37d67cc193a587c6af4edeaaa8ce732f76c97d3
David Wang <djwang826@gmail.com>
2019-12-06 04:02:49 -0500

    Frontend edits and tried to parse data

--- src/content/LandingPage/LandingPage.js
+++ src/content/LandingPage/LandingPage.js
@@ -16,8 +16,9 @@ import Typography from '@material-ui/core/Typography';
 import MenuIcon from '@material-ui/icons/Menu';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import SaveIcon from '@material-ui/icons/Save';
-import EditIcon from '@material-ui/icons/Edit';
 import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Box from '@material-ui/core/Box';
 
 import FlightSegments from '../../components/FlightSegments/FlightSegments';
 import FacebookButton from '../../components/FacebookButton/FacebookButton';
@@ -25,9 +26,11 @@ import FacebookButton from '../../components/FacebookButton/FacebookButton';
 const useStyles = makeStyles(theme => ({
     root: {
         flexGrow: 1,
+        padding: theme.spacing(8, 0, 6),
     },
     menuButton: {
-        marginRight: theme.spacing(2),
+        marginRight: 16,
+        marginLeft: -12,
     },
     rightToolbar: {
         marginLeft: 'auto',
@@ -39,30 +42,94 @@ const useStyles = makeStyles(theme => ({
     container: {
         display: 'flex',
         flexWrap: 'wrap',
-      },
-      textField: {
+    },
+    textField: {
         marginLeft: theme.spacing(1),
         marginRight: theme.spacing(1),
         width: 200,
-      },
+    },
+    heroContent: {
+        padding: theme.spacing(8, 0, 6),
+    },
+    '@global': {
+        ul: {
+            margin: 0,
+            padding: 0,
+        },
+        li: {
+            listStyle: 'none',
+        },
+    },
+    appBar: {
+        borderBottom: `1px solid ${theme.palette.divider}`,
+    },
+    toolbar: {
+        flexWrap: 'wrap',
+    },
+    toolbarTitle: {
+        flexGrow: 1,
+    },
+    link: {
+        margin: theme.spacing(1, 1.5),
+    },
+    footer: {
+        borderTop: `1px solid ${theme.palette.divider}`,
+        marginTop: theme.spacing(8),
+        paddingTop: theme.spacing(3),
+        paddingBottom: theme.spacing(3),
+        [theme.breakpoints.up('sm')]: {
+            paddingTop: theme.spacing(6),
+            paddingBottom: theme.spacing(6),
+        },
+    },
 }));
 
-
+const footers = [
+    {
+        title: 'Company',
+        description: ['Team', 'History', 'Contact us', 'Locations'],
+    },
+    {
+        title: 'Features',
+        description: ['Cool stuff', 'Random feature', 'Team feature', 'Developer stuff', 'Another one'],
+    },
+    {
+        title: 'Resources',
+        description: ['Resource', 'Resource name', 'Another resource', 'Final resource'],
+    },
+    {
+        title: 'Legal',
+        description: ['Privacy policy', 'Terms of use'],
+    },
+];
 
 function classes() {
     useStyles();
     return null;
 }
 
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                411 Project Website made with Material-UI
+        </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
 class LandingPage extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {                        
+        this.state = {
             origin: '',
             destination: '',
             departure: '',
             data: [],
-
+            price: [],
             location: '',
         };
 
@@ -70,21 +137,21 @@ class LandingPage extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleSubmitYelp = this.handleSubmitYelp.bind(this);
 
-        /* Styles */           
+        /* Styles */
         // this.classes = useStyles();    
 
-        /* Classes */         
+        /* Classes */
     }
 
     handleChange(event) {
         // console.log(event.target.name);
-        this.setState({[event.target.name]: event.target.value});
+        this.setState({ [event.target.name]: event.target.value });
     }
-    
+
     handleSubmit(event) {
         event.preventDefault();
 
-        console.log('A request to Amadeus was submitted');  
+        console.log('A request to Amadeus was submitted');
 
         var uri = 'https://test.api.amadeus.com/v1/shopping/flight-offers?origin=MAD&destination=PAR&departureDate=2019-08-01&returnDate=2019-08-28';
         var origin = this.state.origin;
@@ -100,7 +167,7 @@ class LandingPage extends React.Component {
             departureDate: departureDate
         }
 
-        fetch('/amadeus/lowfare',{
+        fetch('/amadeus/lowfare', {
             method: 'POST', // *GET, POST, PUT, DELETE, etc.
             mode: 'cors', // no-cors, *cors, same-origin
             cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
@@ -110,43 +177,22 @@ class LandingPage extends React.Component {
                 // 'Content-Type': 'application/x-www-form-urlencoded',
             },
             body: JSON.stringify(bodyData) // body data type must match "Content-Type" header
-        }).then(res => res.json()).then((response) => {  
-            
+        }).then(results => results.json()).then((response) => {
             var data = []
+            var price = []
+            console.log(response.data)
             response.data.forEach((flightOffer) => {
-                var datum = {}
-                flightOffer.offerItems.forEach((service) => {
-                    service.services.forEach((segment) => {
-                        segment.segments.forEach((flight) => {
-                            Object.keys(flight.flightSegment).forEach((key) => {
-                                datum[key] = flight.flightSegment[key];
-                            })
-                            // datum['pricingDetailPerAdult'] = flight.flightSegment.pricingDetailPerAdult;
-                            // pricingDetailPerAdult
-                            //  - travelClass
-                            //  - fareClass
-                            //  - availability
-                            //  - fareBasis
-                        })                                             
-                    })
-                    // console.log(service)
-                    datum['pricePerAdult'] = service.pricePerAdult;
-                    // price
-                    //  - total
-                    //  - totalTaxes
-                    // pricePerAdult
-                    //  - total
-                    //  - totalTaxes                  
-                })
-                data.push(datum);
-            });
+                price.push(flightOffer.offerItems[0].price.total);
+            })
+            data.push(price);
 
             console.log(data)
-            this.setState({data});
+            this.setState({ data });
 
         }).catch((err) => {
             console.log(err)
-            this.setState({data: []});
+            this.setState({ data: [] });
+            this.setState({ price: [] });
         })
 
         // Convert Data into a list format before inputting into the Table
@@ -156,13 +202,13 @@ class LandingPage extends React.Component {
     handleSubmitYelp(event) {
         event.preventDefault();
 
-        console.log('A request to YELP was submitted');  
+        console.log('A request to YELP was submitted');
 
         fetch('/test').then((data) => {
             console.log(data)
         }).catch((err) => {
-            
-        }) 
+
+        })
 
         var uri = 'https://api.yelp.com/v3/businesses/search';
         var location = this.state.location;
@@ -172,7 +218,7 @@ class LandingPage extends React.Component {
             location: location,
         }
 
-        fetch('/yelp/businesses/search',{
+        fetch('/yelp/businesses/search', {
             method: 'POST', // *GET, POST, PUT, DELETE, etc.
             mode: 'cors', // no-cors, *cors, same-origin
             cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
@@ -188,67 +234,67 @@ class LandingPage extends React.Component {
 
         }).catch((err) => {
             console.log(err)
-            this.setState({data: []});
+            this.setState({ data: [] });
         })
 
     }
 
+    createTable = () => {
+        return (
+            <ol>
+                {this.state.data.map(value => (
+                    <li key={value}>{value}</li>
+                ))}
+            </ol>
+        );
+    }
+
+
+
     render() {
         return (
-            // <div className="container">
-            //     <h1>Our 411 Dashboard</h1>
-            //     <FacebookButton />
-            //     <form onSubmit={this.handleSubmit}>
-            //         <input type="text" name="origin" value={this.state.origin} onChange={this.handleChange} />
-            //         <input type="text" name="destination" value={this.state.destination} onChange={this.handleChange} />
-            //         <input type="text" name="departure" value={this.state.departure} onChange={this.handleChange} />
-            //         <input type="submit" value="Submit" />
-            //     </form>
-            //     <FlightSegments data={this.state.data} />
-            //     {/* <p>{this.state.data}</p> */}
-            //     {/* Example Below on how to add data into a Table Component */}
-            //     {/* <Table headers={this.state.headers} data={this.state.data}></Table> */}
-            //     {/* <Table headers={['header 1','header 2','header 3']} data={[['r1 d1','r1 d2','r1 d3'],['r2 d1','r2 d2','r2 d3'],['r3 d1','r3 d2','r3 d3']]}></Table> */}
-            // </div>
             <>
+
                 <AppBar position="static">
                     <Toolbar>
                         <IconButton edge="start" className={""} color="inherit" aria-label="menu">
-                        <MenuIcon />
+                            <MenuIcon />
                         </IconButton>
                         <Typography variant="h6" className={""}>
-                        411 Dashboard
+                            411 Dashboard
                         </Typography>
-                        <section className={classes.rightToolbar}>
-                            <IconButton color="inherit" aria-label="Save">
-                                <SaveIcon />
-                            </IconButton>
-                            <IconButton color="inherit" aria-label="More Options">
-                                <MoreVertIcon />
-                            </IconButton>
-                            {/* <Button color="inherit">Login</Button> */}
-                            <FacebookButton />
-                        </section>
+                        <IconButton color="inherit" aria-label="Save" align="right">
+                            <SaveIcon />
+                        </IconButton>
+                        <IconButton color="inherit" aria-label="More Options" align="right">
+                            <MoreVertIcon />
+                        </IconButton>
+                        <FacebookButton />
                     </Toolbar>
                 </AppBar>
-                {/*<Container spacing={0} classes={{ root: 'no-padding' }} className={""}>
-                    <Grid> */}
-                        {/* This is where the body of the page begins! */}
-                        {/*<Button color="primary" onClick={() => {fetch('/auth/facebook',{crossDomain:true,}).then(res => res.json()).then(data => console.log('Data:',data)).catch(err => {console.log('Error:',err)})}}>Facebook Login</Button>
-                    </Grid>*/}
-                {/*</Container>*/}
+
+                {/* Hero unit */}
+                <Container maxWidth="sm" component="main" className={classes.heroContent}>
+                    <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+                        Travel Aid
+        </Typography>
+                    <Typography variant="h5" align="center" color="textSecondary" component="p">
+                        An easy to use web application that can help you find the cheapest flights instantly, along with finding the best tourist attractions and buisnesses to visit at your destination!
+        </Typography>
+                </Container>
+                {/* End hero unit */}
 
                 {/* Form For Low Flight Fare */}
-                <form className={classes.root} noValidate autoComplete="off" onSubmit={this.handleSubmit}>
+                <form className={classes.root} noValidate autoComplete="off" onSubmit={this.handleSubmit} align="center">
                     {/*Start and End Location Forms */}
                     <TextField id="standard-basic" name="origin" value={this.state.origin} onChange={this.handleChange} label="Start Location" />
                     <TextField id="standard-basic" name="destination" value={this.state.destination} onChange={this.handleChange} label="End Location" />
 
                     {/* Date and Time pickers */}
-                    <TextField                         
+                    <TextField
                         id="date"
-                        name="departure" 
-                        value={this.state.departure} 
+                        name="departure"
+                        value={this.state.departure}
                         onChange={this.handleChange}
                         label="Start Date"
                         type="date"
@@ -258,26 +304,14 @@ class LandingPage extends React.Component {
                             shrink: true,
                         }}
                     />
-                    {/* <TextField
-                        id="date"
-                        name="return" 
-                        value={this.state.departure} 
-                        onChange={this.handleChange}
-                        label="Return Date"
-                        type="date"
-                        defaultValue="2019-01-01"
-                        className={classes.textField}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    /> */}
 
                     {/* Submit Button */}
                     <Button type="submit" value="Submit" color="primary">Submit</Button>
 
                 </form>
 
-                <form className={classes.root} noValidate autoComplete="off" onSubmit={this.handleSubmitYelp}>
+
+                <form className={classes.root} noValidate autoComplete="off" onSubmit={this.handleSubmitYelp} align="center">
                     {/*Start and End Location Forms */}
                     <TextField name="location" value={this.state.location} onChange={this.handleChange} label="Address" />
 
@@ -286,9 +320,35 @@ class LandingPage extends React.Component {
 
                 </form>
 
-                <p>{JSON.stringify(this.state.data)}</p>
-                
-            </>          
+                {this.createTable()}
+
+                {/* Footer */}
+                <Container maxWidth="md" component="footer" className={classes.footer}>
+                    <Grid container spacing={4} justify="space-evenly">
+                        {footers.map(footer => (
+                            <Grid item xs={6} sm={3} key={footer.title}>
+                                <Typography variant="h6" color="textPrimary" gutterBottom>
+                                    {footer.title}
+                                </Typography>
+                                <ul>
+                                    {footer.description.map(item => (
+                                        <li key={item}>
+                                            <Link href="#" variant="subtitle1" color="textSecondary">
+                                                {item}
+                                            </Link>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </Grid>
+                        ))}
+                    </Grid>
+                    <Box mt={5}>
+                        <Copyright />
+                    </Box>
+                </Container>
+                {/* End footer */}
+
+            </>
         );
     }
 }
